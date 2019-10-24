bcc986260fc41e31c2b343e567bd97b925e7d9b9
piaofeifengxinzi <2310011026@qq.com>
2019-10-24 19:30:11 +0800

    firstcommit

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/TimePickerFragment.java
@@ -0,0 +1,77 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TimePicker;
+
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class TimePickerFragment extends DialogFragment {
+
+    public static final String EXTRA_TIME =
+            "com.bignerdranch.android.criminalintent.time";
+
+    private static final String ARG_TIME = "time";
+
+    private TimePicker timePicker;
+
+    public static TimePickerFragment newInstance(Date date) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_TIME, date);
+        TimePickerFragment fragment = new TimePickerFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        Date date = (Date)getArguments().getSerializable(ARG_TIME);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int hour = calendar.get(Calendar.HOUR);
+        int minute = calendar.get(Calendar.MINUTE);
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_time, null);
+        timePicker = (TimePicker)v.findViewById(R.id.dialog_time_picker);
+        return new AlertDialog.Builder(getActivity())
+                .setView(v)
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @RequiresApi(api = Build.VERSION_CODES.M)
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        int hour = timePicker.getHour();
+                        int minute = timePicker.getMinute();
+                        Date date1 = new GregorianCalendar(year,month,day,hour,minute).getTime();
+                        sendResult(Activity.RESULT_OK, date1);
+                    }
+                })
+                .create();
+    }
+
+    private void sendResult(int resultCode, Date date) {
+        if (getTargetFragment() == null) {
+            return;
+        }
+
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_TIME, date);
+
+        getTargetFragment()
+                .onActivityResult(getTargetRequestCode(), resultCode, intent);
+    }
+}
